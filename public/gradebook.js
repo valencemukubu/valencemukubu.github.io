ba15ab2d56d1c828c7d53218548d807c15e3533b
valencemukubu <vmukubu@my.waketech.edu>
2025-04-11 21:42:08 -0400

    Add files via upload

---
+++ public/gradebook.js
@@ -0,0 +1,14 @@
+// TODO Fetch data from the PortgreSQL database(to be implemented later)
+Function fetchGradebook() {
+      // This function will query the PostgreSQL database and return grade data
+    console.log("Fetching grade data...");
+}
+// TODO: Populate the table with grade data
+function populateGradebook(data) {
+  console.log("Populating gradebook with data:", data);
+}
+// TODO REMOVE THIS
+// Call the stubs to demonstrate the workfloww
+const gradeData = fetchGradeData();
+populateGradebook(gradeData)
+//END Remove
